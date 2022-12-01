4fdce7c366405246ab83cb8173f3d77af620e53e
fabiooo4 <fabioirimie@gmail.com>
2022-12-01 13:27:00 +0100

    added ball array with random arguments

--- sketch.js
+++ sketch.js
@@ -8,8 +8,14 @@ class Ball {
   }
 
   draw() {
-    fill(255);
-    stroke(255);
+    color = {
+      r: random(255),
+      g: random(255),
+      b: random(255)
+    };
+
+    fill(color.r, color.g, color.b);
+    stroke(color.r, color.g, color.b);
     ellipse(this.x, this.y, this.size);
   }
 
@@ -28,16 +34,21 @@ class Ball {
   }
 } 
 
-let ball = new Ball(100, 100, 30, 7, 5);
+let balls = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 1000; i++) {
+    balls.push(new Ball(random(width), random(height), random(10, 50), random(-5, 5), random(-5, 5)));
+  }
 }
 
 function draw() {
   background(0);
 
-  ball.draw();
-  ball.move();
-  ball.bounce();
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].move();
+    balls[i].bounce();
+  }
 }
