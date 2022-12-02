774298877bfe13e19198bf83856e8335bbda3e5f
fabiooo4 <fabioirimie@gmail.com>
2022-12-02 22:41:09 +0100

    removed ball out of bounds when bouncing

--- sketch.js
+++ sketch.js
@@ -22,10 +22,10 @@ class Ball {
   }
 
   bounce() {
-    if (this.pos.x > width || this.pos.x < 0) {
+    if (this.pos.x > width - this.size || this.pos.x < 0 + this.size) {
       this.vel.x *= -1;
     }
-    if (this.pos.y > height || this.pos.y < 0) {
+    if (this.pos.y > height - this.size || this.pos.y < 0 + this.size) {
       this.vel.y *= -1;
     }
   }
