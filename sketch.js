41ccfc00d2febf28ad19af9b1ebb87e8ef39602e
fabiooo4 <fabioirimie@gmail.com>
2022-12-02 22:22:30 +0100

    replaced position and velocity variables with vectors

--- sketch.js
+++ sketch.js
@@ -1,35 +1,32 @@
 class Ball {
-  constructor(x, y, size, ySpeed, xSpeed) {
-    this.x = x;
-    this.y = y;
+  constructor(pos, vel, size) {
+    this.pos = pos;
     this.size = size;
-    this.xSpeed = ySpeed;
-    this.ySpeed = xSpeed;
+    this.vel = vel;
   }
 
   draw() {
     color = {
-      r: random(255),
-      g: random(255),
-      b: random(255)
+      r: 255,
+      g: 255,
+      b: 255
     };
 
     fill(color.r, color.g, color.b);
     stroke(color.r, color.g, color.b);
-    ellipse(this.x, this.y, this.size);
+    ellipse(this.pos.x, this.pos.y, this.size);
   }
 
   move() {
-    this.x += this.xSpeed;
-    this.y += this.ySpeed;
+    this.pos.add(this.vel);
   }
 
   bounce() {
-    if (this.x > width || this.x < 0) {
-      this.xSpeed *= -1;
+    if (this.pos.x > width || this.pos.x < 0) {
+      this.vel.x *= -1;
     }
-    if (this.y > height || this.y < 0) {
-      this.ySpeed *= -1;
+    if (this.pos.y > height || this.pos.y < 0) {
+      this.vel.y *= -1;
     }
   }
 } 
@@ -38,13 +35,14 @@ let balls = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < 1000; i++) {
-    balls.push(new Ball(random(width), random(height), random(10, 50), random(-5, 5), random(-5, 5)));
-  }
+  let pos = createVector(random(width), random(height));
+  let vel = createVector(random(5, 10), random(5, 10));
+
+  balls.push(new Ball(pos, vel, 25)); 
 }
 
 function draw() {
-  background(0);
+  background(0, 25);
 
   for (let i = 0; i < balls.length; i++) {
     balls[i].draw();
